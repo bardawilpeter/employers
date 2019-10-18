c527fcb6ca9e63cb6212880628f3e610dccef42f
Peter Bardawil <peter.bardawil@cleartag.local>
2019-10-18 23:42:24 +0300

    Add validation to employee creation
      - Using validator module
      - add all member field in validation

--- src/schema/employees/resolvers.ts
+++ src/schema/employees/resolvers.ts
@@ -1,3 +1,5 @@
+import validator = require('validator');
+
 // App Imports
 import { Employee } from '../../models/employee';
 
@@ -7,8 +9,13 @@ import { Employee } from '../../models/employee';
    * @return {id:createdEmployee._id,name:createdEmployee.name,email:createdUser.email,location:createdEmployee.location,department:createdEmployee.department,imageUrl:createdEmployee.imageUrl} The created employee.
 */
 export async function addEmployee(parentValue: any, args: any, req: any) {
-  const errors = [];
-
+  console.log("-------------");
+  const errors = validateEmployee(args);
+  console.log(errors);
+  if (errors.length > 0) {
+    const error = new Error('Invalid input.');
+    throw error;
+  }
   const employee = new Employee({
     name: args.name,
     email: args.email,
@@ -17,4 +24,25 @@ export async function addEmployee(parentValue: any, args: any, req: any) {
     imageUrl: args.imageUrl
   });
   return await employee.save();
+}
+
+export function validateEmployee(args: any) {
+  const errors = [];
+  if (validator.isEmpty(args.name))
+  {
+    errors.push({ message: 'Title is invalid.' });
+  }
+  if (validator.isEmpty(args.email) || !validator.isEmail(args.email))
+  {
+    errors.push({ message: 'Email is invalid.' });
+  }
+  if (validator.isEmpty(args.location))
+  {
+    errors.push({ message: 'Location is invalid.' });
+  }
+  if (validator.isEmpty(args.department))
+  {
+    errors.push({ message: 'Department is invalid.' });
+  }
+  return errors;
 }
