3de4dfca3107bed652b085b2b7ca21c090545a8a
Peter Bardawil <peter.bardawil@cleartag.local>
2019-10-19 09:45:06 +0300

    Add employee list logic under getEmployees
     - return employees list
     - return total number of employees
     - TODO ability to search for employees by name

--- src/schema/employees/resolvers.ts
+++ src/schema/employees/resolvers.ts
@@ -46,10 +46,28 @@ export async function setEmployee(parentValue: any, args: any, req: any) {
 /**
    * Get Employees.
    * @param {args} - containing params sent by graphql expecting (page).
-   * @return {id:createdEmployee._id,name:createdEmployee.name,email:createdUser.email,location:createdEmployee.location,department:createdEmployee.department,imageUrl:createdEmployee.imageUrl} The updated employee.
+   * @return {employeesList:employeesList,totalEmployees:totalEmployees} contain employees list with total number of employees.
 */
 export async function getEmployees(parentValue: any, args: any, req: any) {
-
+  //TODO add ability to search in post
+  const page = (!args.page)?1:args.page;
+  const perPage = 2;
+  const totalEmployees = await Employee.find().countDocuments();
+  const employees = await Employee.find()
+    .sort({ createdAt: -1 })
+    .skip((page - 1) * perPage)
+    .limit(perPage);
+  return {
+    employeesList: employees.map((employee:any) => {
+      return {
+        ...employee._doc,
+        _id: employee._id.toString(),
+        createdAt: employee.get("createdAt").toISOString(),//TODO fix attribute to get from document
+        updatedAt: employee.get("updatedAt").toISOString()//TODO fix attribute to get from document
+      };
+    }),
+    totalEmployees: totalEmployees
+  };
 }
 
 /**
