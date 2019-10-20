a0007a7bb4d287f0278a57bab1d2f154cead5592
Peter Bardawil <peter.bardawil@cleartag.local>
2019-10-20 10:02:15 +0300

    Authentication page creation:
     - add Auth holder for login form
     - add login page form

---
+++ src/pages/Auth/Auth.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const auth = props => <section className="auth-form-holder">{props.children}</section>;
+
+export default auth;
---
+++ src/pages/Auth/Login.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import Auth from './Auth';
+
+class Login extends Component {
+    render() {
+      return (
+        <Auth>
+          <form>
+            
+          </form>
+        </Auth>
+      );
+    }
+  }
+  
+  export default Login;
